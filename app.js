296679875e23657751382f8cdc379d5c19da2426
pooltoneraa <pool.toneraa@gmail.com>
2026-05-08 15:18:02 +0200

    eliminar

--- app.js
+++
@@ -1,9 +0,0 @@
-function go(pagina) {
-  if (pagina === 'internet') {
-    window.location.href = 'internet.html';
-  } else if (pagina === 'datos') {
-    alert('Página de datos aún no creada');
-  } else if (pagina === 'llamadas') {
-    alert('Página de llamadas aún no creada');
-  }
-}
